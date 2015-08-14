e4e196debd0ba53472c60a16391717d1604b6b64
Nathaniel Jones <nljones@mit.edu>
2015-08-14 17:06:49 -0400

    Updated OptiX isotropsky to match method signatures of other callable programs

--- src/rt/isotropsky.cu
+++ src/rt/isotropsky.cu
@@ -17,7 +17,7 @@ rtDeclareVariable(Transform,  transform, , ); /* transformation matrix, ignored 
 
 // Calculate the isotropic sky value for the current ray direction.
 // This function replicates the algorithm in isotrop_sky.cal distributed with Daysim.
-RT_CALLABLE_PROGRAM float isotrop_sky( const float3 direction )
+RT_CALLABLE_PROGRAM float3 isotrop_sky(const float3 ignore0, const float3 ignore1)
 {
-	return skybright;
+	return make_float3(skybright);
 }
